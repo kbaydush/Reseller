919f8c053f7ce0fa229386c296fe36ff9d37fafe
Sergiy Shvets <sshvets@magento.com>
2016-01-19 15:47:04 +0200

    added fabric for Handler

--- sync.php
+++ sync.php
@@ -4,26 +4,9 @@
 require_once dirname(__FILE__) . "/bootstrap.php";
 
 if (isset($argv[1])) {
-    $command = $argv[1];
 
-    switch ($command) {
-        case "one":
-            $Handler = new Handler_Http($CFG, $CFG->get('all_form_id_array')['purchases']);
-            $Handler->setRequestParams($_GET, $_POST);
-            break;
-        case "two":
-            $Handler = new Handler_Http($CFG, $CFG->get('all_form_id_array')['refunds']);
-            $Handler->setRequestParams($_GET, $_POST);
-            break;
-        case "cron":
-            $Handler = new Handler_Cron($CFG, 'cron');
-            break;
-        default:
-            throw new \InvalidArgumentException("bad parameter to run ( one, two, cron )");
-    }
-
-    $Handler->action();
-
+    HandlerFabric::handle($argv[1], $CFG)
+        ->action();
 
 } else {
     throw new \InvalidArgumentException("need one parameter to run ( one, two, cron )");
